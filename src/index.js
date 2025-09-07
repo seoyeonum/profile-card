5b05b5e27f07fdd4a9769d99895dc0e23ec44ee4
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-09-07 21:21:14 +0900

    ✨ feat: add Avatar and Intro components
    - create reusable components
    - add the profile photo file
    - display profile photo, name, and bio
    - prepare SkillList and Skill components to express skills

--- src/index.js
+++ src/index.js
@@ -4,15 +4,39 @@ import './style.css';
 
 function App() {
   return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="SeoyeonUm.jpg" alt="Seoyeon Um" />;
+}
+
+function Intro() {
+  return (
     <div>
-      <h1>Hello React!</h1>
+      <h1>Seoyeon Um</h1>
       <p>
-        Let's make a profile card with React. <br />
-        리액트로 프로필 카드 만들기.
+        Front-end web developer and student. When not coding or taking a course,
+        I like to read some books, to enjoy K-pop, or to just hang out with
+        childhood friends in my neck of the woods.
       </p>
     </div>
   );
 }
+
+function SkillList() {}
+
+function Skill() {}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
